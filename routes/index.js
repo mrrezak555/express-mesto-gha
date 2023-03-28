57cd4b5d50cc53a9404e678dd857d3a1112b104f
Kirill Zuev <mrrezak555@yandex.ru>
2023-03-28 19:10:30 +0300

    3

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 
+const NOT_FOUND = 404;
+
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
 router.use((req, res) => {
-  res.status(404).send({ message: 'Проверьте корректность пути запроса' });
+  res.status(NOT_FOUND).send({ message: 'Проверьте корректность пути запроса' });
 });
 
 module.exports = router; // экспортировали роуте
